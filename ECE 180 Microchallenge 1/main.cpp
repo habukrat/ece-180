d03eb238596f0a84de8dc26efbe0a8b4207fe878
habukrat <habukrat@ucsd.edu>
2018-01-10 20:01:18 -0800

    Initial Commit

---
+++ ECE 180 Microchallenge 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ECE 180 Microchallenge 1
+//
+//  Created by Chaim A on 1/10/18.
+//  Copyright (c) 2018 Hadar Abukrat. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
